c66693b397da17888ae55e330752b7e24244bb36
apply2 <67138665+apply2@users.noreply.github.com>
2026-03-27 17:51:55 +0900

    Update 4-1.c

--- code/4-1/4-1.c
+++ code/4-1/4-1.c
@@ -7,13 +7,13 @@ int main() {
         printf("%d ", a[i]);
     printf("\n");
 
-    int b[2] = {1, 2};
+    int b[5] = {1, 2};
 
     for (int i = 0; i < 5; i++)
         printf("%d ", b[i]);
     printf("\n");
 
-    int c[2] = {0};
+    int c[5] = {0};
 
     for (int i = 0; i < 5; i++)
         printf("%d ", c[i]);
