b989eb20df2d699d42952706f31854060957dd0e
apply2 <67138665+apply2@users.noreply.github.com>
2026-03-27 15:16:34 +0900

    Update 1-2.c

--- code/1-2/1-2.c
+++ code/1-2/1-2.c
@@ -2,14 +2,10 @@
 #include "util.h"
 
 int main() {
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < 100; i++)
         if (i%2 == 0)
-        {
             logger("even")
-        }
-        else {
+        else
             logger("odd")
-        }
-    }
     return 0;
 }
